0262f753c8f4b1e4eda255dba39e287e855cd47b
e215751 <e215751@ie.u-ryukyu.ac.jp>
2021-12-20 21:21:33 +0900

    step3

--- Report5.java
+++ Report5.java
@@ -2,15 +2,7 @@ import java.io.*;
 
 public class Report5{
     public static void main(String[] args){
-        try{
-            String str = null;
-        System.out.println(str);
-        System.out.println(str.length());
-
-        }catch(NullPointerException e){
-            System.out.println("エラー発生！変数がnullだよ！");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
